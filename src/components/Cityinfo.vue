44ba976ee8e60f668ca9121b56b0bbe9b1538674
DenisKostyukov <68915197+DenisKostyukov@users.noreply.github.com>
2020-11-16 15:46:15 +0200

    Day 5

--- src/components/Cityinfo.vue
+++ src/components/Cityinfo.vue
@@ -1,13 +1,38 @@
 <template>
   <div>
-    {{ weather }}
+    <p>Погода</p>
+
+    <p>
+      <span>Город: </span><span>{{ weather.name }} </span>
+    </p>
+    <p>
+      <span>Описание: </span><span>{{ weather.weather[0].description }} </span>
+    </p>
+    <p>
+      <span>Влажность:</span><span>{{ weather.main.humidity }}</span>
+    </p>
+    <p>
+      <span>Температура:</span><span>{{ weather.main.temp }}</span>
+    </p>
+    <p>
+      <span>Ощущается:</span><span>{{ weather.main.feels_like }}</span>
+    </p>
+    <p>
+      <span>Давление:</span><span>{{ weather.main.pressure }}</span>
+    </p>
+    <p>
+      <span>Скорость ветра:</span><span>{{ weather.wind.speed }}</span>
+    </p>
   </div>
 </template>
 <script>
 import Vue from "vue";
 export default {
   props: {
-    name: "",
+    name: {
+      type: String,
+      required: true,
+    },
   },
   data: function () {
     return {
@@ -19,7 +44,7 @@ export default {
       .get(
         "http://api.openweathermap.org/data/2.5/find?q=" +
           this.name +
-          "&appid=4f1eefd698127c5ee5b4f3ee7040f4bc&units=metric"
+          "&appid=4f1eefd698127c5ee5b4f3ee7040f4bc&units=metric&lang=ru"
       )
       .then((response) => {
         console.log(response.data.list);
--- src/components/Weather.vue
+++ src/components/Weather.vue
@@ -11,18 +11,37 @@
           {{ city }}
         </option></select
       ><br /><br />
-
       <button>
         <router-link v-bind:to="'/city-info/' + selectCity"
           >Weather
         </router-link>
       </button>
-      <button @click="cityWeather()">city</button>
+
       <br />
       <p>Погода</p>
-      {{ latitude }}
-      {{ longitude }}
-      {{ weather }}
+
+      <p>
+        <span>Город: </span><span>{{ weather.name }} </span>
+      </p>
+      <p>
+        <span>Описание: </span
+        ><span>{{ weather.weather[0].description }} </span>
+      </p>
+      <p>
+        <span>Влажность:</span><span>{{ weather.main.humidity }}</span>
+      </p>
+      <p>
+        <span>Температура:</span><span>{{ weather.main.temp }}</span>
+      </p>
+      <p>
+        <span>Ощущается:</span><span>{{ weather.main.feels_like }}</span>
+      </p>
+      <p>
+        <span>Давление:</span><span>{{ weather.main.pressure }}</span>
+      </p>
+      <p>
+        <span>Скорость ветра:</span><span>{{ weather.wind.speed }}</span>
+      </p>
     </div>
   </div>
 </template>
@@ -33,54 +52,42 @@ export default {
   data: function () {
     return {
       weather: {},
-      cities: ["Kyiv", "Zaporizhzhia"],
+      cities: ["Kyiv"],
       city: "",
       selectCity: "",
-      latitude: "",
-      longitude: "",
-      position: "",
     };
   },
-  beforeMount: function () {
-    if (navigator.geolocation) {
-      navigator.geolocation.getCurrentPosition(this.setlocation);
-    } else {
-      this.position = "Geolocation is not supported by this browser.";
-    }
-  },
+
   mounted: function () {
-    Vue.axios
-      .get(
-        "http://api.openweathermap.org/data/2.5/find?lat=" +
-          this.latitude +
-          "&lon=" +
-          this.longitude +
-          "&cnt=10&appid=4f1eefd698127c5ee5b4f3ee7040f4bc&units=metric"
-      )
-      .then((response) => {
-        console.log(response.data.list);
-        this.weather = response.data.list[0];
+    this.cities = JSON.parse(localStorage.getItem("cities"));
+    var getPosition = function (options) {
+      return new Promise(function (resolve, reject) {
+        navigator.geolocation.getCurrentPosition(resolve, reject, options);
+      });
+    };
+    getPosition()
+      .then((position) => {
+        Vue.axios
+          .get(
+            "http://api.openweathermap.org/data/2.5/find?lat=" +
+              position.coords.latitude +
+              "&lon=" +
+              position.coords.longitude +
+              "&cnt=3&appid=4f1eefd698127c5ee5b4f3ee7040f4bc&units=metric&lang=ru"
+          )
+          .then((response) => {
+            console.log(response.data.list);
+            this.weather = response.data.list[1];
+          });
+      })
+      .catch((err) => {
+        console.error(err.message);
       });
   },
   methods: {
-    setlocation: function (position) {
-      this.latitude = position.coords.latitude;
-      this.longitude = position.coords.longitude;
-    },
     addNew: function () {
       this.cities.push(this.city);
-    },
-    cityWeather: function () {
-      Vue.axios
-        .get(
-          "http://api.openweathermap.org/data/2.5/find?q=" +
-            this.selectCity +
-            "&appid=4f1eefd698127c5ee5b4f3ee7040f4bc&units=metric"
-        )
-        .then((response) => {
-          console.log(response.data.list);
-          this.weather = response.data.list[0];
-        });
+      localStorage.setItem("cities", JSON.stringify(this.cities));
     },
   },
 };
